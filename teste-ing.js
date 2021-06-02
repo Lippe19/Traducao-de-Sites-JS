5233f7a280728799dfe32d8dd9b58b6a7de48f5d
Lippe19 <luizphilippe.sni@gmail.com>
2021-06-02 17:56:32 -0300

    primeiro commit

---
+++ teste-ing.js
@@ -0,0 +1,17 @@
+var ing={
+    data:function(){
+        return{
+            ing:{
+                linguagem:'ing',
+                
+                pages:{
+                    index:{
+                        img:'images/ING/capa_home.svg',
+                        subtitulo:"The GR GROUP's main purpose is to make people happy by means of positive and lasting experiences, with transparency in relationships, teamwork, commitment to sustainability and a lot of enthusiasm to go a step further.",
+                        subsubtitulo:"The Group has 20 yearsâ€™ experience in the tourism and entertainment industry, with a number of hotels, resorts, theme parks and tourist attractions spread all across the countryâ€™s main destinations. Its portfolio also includes the largest hotel complex in Brazil along with extensive knowledge of the timeshare industry."
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ teste-ptb.js
@@ -0,0 +1,17 @@
+var ptbr = {
+    data: function () {
+        return { 
+            ptbr: {
+                linguagem: 'ptbr',
+
+                pages: { 
+                    index: { 
+                        img: 'images/PTBR/capa_home.svg', 
+                        subtitulo: 'O GR GROUP tem como propÃ³sito principal fazer as pessoas felizes a partir de experiÃªncias positivas e duradouras, com transparÃªncia nas relaÃ§Ãµes, trabalho em equipe, comprometimento com a sustentabilidade e muito entusiasmo para ir alÃ©m.', 
+                        subsubtitulo: 'HÃ¡ 20 anos atuando na indÃºstria do turismo e entretenimento, com diversos hotÃ©is, resorts, parques temÃ¡ticos e atraÃ§Ãµes turÃ­sticas espalhados nos principais destinos do paÃ­s, possui em seu portfolio o maior complexo hoteleiro do Brasil e amplo conhecimento na industria de tempo compartilhado.'
+                    } 
+                }
+            }
+        }
+    }
+} 
